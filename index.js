08a58644e19d32d4598a13de9ff852be36441bdc
oscmasa <oscar.sanchezduran1@gmail.com>
2023-08-03 16:18:42 -0500

    Buscar Ciudad Completado

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
-const Busquedas = require("./models/busquedas");
+require('dotenv').config()
 
+const { leerInput, inquirerMenu, pausa, listadoLugares } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 const main = async () => {
 
@@ -15,23 +16,28 @@ const main = async () => {
 
             case 1:
                 // Mostrar mensaje
-                const lugar = await leerInput('Ciudad: ');
-                await busquedas.ciudad( lugar );
+                const termino = await leerInput('Ciudad: ');
 
                 // Buscar los lugares
+                const lugares = await busquedas.ciudad( termino );
 
                 // Seleccionar el lugar
+                const id = await listadoLugares(lugares);
+                const lugarSel = lugares.find( l => l.id === id );
 
                 // Clima
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
 
                 // Mostrar resu;tados
+                console.clear();
                 console.log('\nInfo de la ciudad\n'.green);
-                console.log('Ciudad:', );
-                console.log('Lat:', );
-                console.log('Lng:', );
-                console.log('Temperatura:', );
-                console.log('Minima:', );
-                console.log('Maxima:', );
+                console.log('Ciudad:', lugarSel.nombre.green);
+                console.log('Lat:', lugarSel.lat);
+                console.log('Lng:', lugarSel.lng);
+                console.log('Temperatura:', clima.temp);
+                console.log('Minima:', clima.min);
+                console.log('Maxima:', clima.max);
+                console.log('El clima esta:', clima.desc.green);
 
                 break;
 
--- models/busquedas.js
+++ models/busquedas.js
@@ -10,8 +10,16 @@ class Busquedas {
     get paramsMapbox() {
         return {
             'language': 'es',
-            'access_token': 'pk.eyJ1IjoiYnQtMTExIiwiYSI6ImNsa3VlbmcwdDAzYzgzZXBpb3dwd2E2MnIifQ.lCq-219PfuOj0Jn3lVgVew',
+            'access_token': process.env.MAPBOX_KEY,
             'limit': 5
+        }
+    }
+
+    get paramsOpenWeather() {
+        return {
+            appid: process.env.OPENWEATHER_KEY,
+            units: 'metric',
+            lang: 'es'
         }
     }
 
@@ -25,12 +33,39 @@ class Busquedas {
             });
 
             const resp = await instance.get();
-            console.log(resp.data);
-
-            return []
+            return resp.data.features.map( lugar => ({
+                id: lugar.id,
+                nombre: lugar.place_name,
+                lng: lugar.center[0],
+                lat: lugar.center[1]
+            }));
             
         } catch (error) {
             return [];
+        }
+    }
+
+    async climaLugar( lat, lon ) {
+
+        try{
+
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                params: { ...this.paramsOpenWeather, lat,lon }
+            });
+
+            const resp = await instance.get();
+            const { weather, main } = resp.data;
+
+            return {
+                desc: weather[0].description,
+                temp: main.temp,
+                max: main.temp_max,
+                min: main.temp_min
+            }
+
+        } catch (error) {
+            console.log(error);
         }
     }
 }
